3a8ea1cf1941a947aa53cdf6a959db57e3388068
TaiseiShinozaki <shinozakitaisei00@gmail.com>
2020-11-22 20:15:25 +0900

    viewの変更

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -25,6 +25,16 @@ class ContentsController < ApplicationController
     @comments = @content.comments.includes(:user).order("created_at DESC")
   end
 
+  def destroy
+    @content = Content.find(params[:id])
+    if current_user.id == @content.user.id
+      @content.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   private
   def move_to_root_path
     redirect_to root_path if user_signed_in? == false
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :content
   belongs_to :user
+
 end
